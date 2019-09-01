d350470baa874fc5e9af6d0d2cfe11a17082f2f0
wgrey <609042054@qq.com>
2019-09-01 15:07:54 +0800

    提交项目到版本库

---
+++ git-test-project/src/com/atheima/git/TestGit.java
@@ -0,0 +1,13 @@
+package com.atheima.git;
+
+public class TestGit {
+    public static void main(String[] args) {
+        System.out.println("测试git");
+        System.out.println("测试git");
+        System.out.println("测试git");
+        System.out.println("测试git");
+        System.out.println("测试git");
+        System.out.println("测试git");
+        System.out.println("测试git");
+    }
+}
